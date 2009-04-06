1f35bbfef11cfb99adec084b5f2548603a80fc74
jpablo <jpablo@a905e4cf-975a-0410-83e3-fddb0941942b>
2009-04-06 06:13:36 +0000

    Alabeada es una "pagina" que contiene al hijo "alabeada"

--- Alabeada.py
+++ Alabeada.py
@@ -4,15 +4,21 @@ from pivy.coin import *
 from pivy.gui.soqt import *
 from PyQt4 import QtCore, QtGui
 from math import *
-from superficie.VariousObjects import Sphere, Tube, Line, Bundle, Base
+from superficie.VariousObjects import Sphere, Tube, Line, Bundle, Base, Page
 from superficie.util import intervalPartition, wrap, Vec3, conectaParcial,  \
     genIntervalPartition, partial, conecta
 from superficie.gui import onOff, CheckBox, Slider
 from superficie.animation import Timer
 
 
-class Alabeada(Base):
-    def __init__(self):
+class Alabeada(Page):
+    def __init__(self,parent = None):
+        Page.__init__(self, parent, "Alabeada")
+        self.addChild(alabeada())
+
+
+class alabeada(Base):
+    def __init__(self, parent=None):
         Base.__init__(self, "Alabeada")
         ## ============================
         c   = lambda t: Vec3(t,t**2,t**3)
