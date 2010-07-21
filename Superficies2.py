d91851da7612edd7b7f3426ec04db315f793fb9f
donaji <donaji@a905e4cf-975a-0410-83e3-fddb0941942b>
2010-07-21 20:16:06 +0000

    cambios en plano, falta la circunferencia

--- Superficies2.py
+++ Superficies2.py
@@ -14,6 +14,7 @@ from superficie.VariousObjects import BasePlane,Line
 from superficie.Book import Chapter, Page
 from superficie.Plot3D import Plot3D, RevolutionPlot3D
 from superficie.util import _1
+from superficie.util import intervalPartition
 
 
 class Plano(Page):
@@ -27,37 +28,67 @@ class Plano(Page):
         plano.setSpecularColor(_1(189, 121 , 106 ))
         self.setupPlanes((-2,2,7))
 
-        p_1 = Sphere((1, 2, -1./3),0.02,visible=True)
-        p_1.setColor( _1(116,112,35))
+        p_1 = Sphere((2, -2, 2),0.02,visible=True)
+        p_1.setColor( _1(214,44,109))
         p_1.setShininess(1)
-        
-        p_2 = Sphere((2, -2, 2),0.02,visible=True)
-        p_2.setColor( _1(214,44,109))
+
+        p_2 = Sphere((1, 2, -1./3),0.02,visible=True)
+        p_2.setColor( _1(116,112,35))
         p_2.setShininess(1)
         
 #        pun2_1 = [[-3t/2, sin(t), t] for t in intervalPartition((tmin, tmax, npuntos))]
 #        curva = Line(puntos,(1, 1, 1), 2,parent=self, nvertices=1)
 #        recta2_1
 
-        p_3 = Sphere((-1, 1, 1),0.02,visible=True)
+        p_3 = Sphere((-1, -1, 7./3),0.02,visible=True)
         p_3.setColor( _1(52,171,215))
         p_3.setShininess(1)
 
+#       Calculando las curvas que pasan por los 3 puntos anteriores.
+
+        npuntos = 20
+
+        tmin1_3 = 1.1
+        tmax1_3 = 2.
+
         puntos1_1 = [ (2,-2,2), (2,-1,4./3) ]
         puntos1_2 = [ (2,-2,2), (1.5,1,1./6) ]
+        puntos1_3 = [[t, -1./4*t**3., 1./3*(4-t+1./2*t**3.)] for t in intervalPartition((tmin1_3, tmax1_3, npuntos))]
 
         curva1_1 = Line(puntos1_1, (1, 0, 0), 2, parent=self, nvertices=1)
         curva1_2 = Line(puntos1_2, (0, 0, 1), 2, parent=self, nvertices=1)
+        curva1_3 = Line(puntos1_3, (0, 1, 0), 2, parent=self, nvertices=1)
+
+        tmin2_3 = 0.
+        tmax2_3 = 1.
+
+        puntos2_1 = [(1, 2, -1./3),(0,1,2./3)]
+        puntos2_2 = [(1, 2, -1./3),(1,0,1)]
+        puntos2_3 = [[t, 2*t**3., 1./3*(4-t-4*t**3.)] for t in intervalPartition((tmin2_3, tmax2_3, npuntos))]
+
+        curva2_1 = Line(puntos2_1, (1, 0, 0), 2, parent=self, nvertices=1)
+        curva2_2 = Line(puntos2_2, (0, 0, 1), 2, parent=self, nvertices=1)
+        curva2_3 = Line(puntos2_3, (0, 1, 0), 2, parent=self, nvertices=1)
+
+        tmin3_3 = -0.5
+        tmax3_3 = -1.
+
+        puntos3_1 = [(-1, -1, 7./3),(0.8,0.8,8./15)]
+        puntos3_2 = [(-1, -1, 7./3),(-2,-1,8./3)]
+        puntos3_3 = [[t, t**3., 1./3*(4-t-2*t**3.)] for t in intervalPartition((tmin3_3, tmax3_3, npuntos))]
+
+        curva3_1 = Line(puntos3_1, (1, 0, 0), 2, parent=self, nvertices=1)
+        curva3_2 = Line(puntos3_2, (0, 0, 1), 2, parent=self, nvertices=1)
+        curva3_3 = Line(puntos3_3, (0, 1, 0), 2, parent=self, nvertices=1)
 
         self.addChild(plano)
         self.addChild(p_1)
         self.addChild(p_2)
         self.addChild(p_3)
         
-        self.setupAnimations([curva1_2,curva2_2])
-
-        self.setupAnimations([curva1_1, curva1_2])
-
+        self.setupAnimations([curva1_1,curva1_2,curva1_3,curva2_1,curva2_2,curva2_3,curva3_1,curva3_2,curva3_3])
+#        self.setupAnimations([curva2_1,curva2_2])
+ #       self.setupAnimations([curva3_1,curva3_2])
 
 class ParaboloideEliptico(Page):
     def __init__(self):
