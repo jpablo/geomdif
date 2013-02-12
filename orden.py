b298dbcd2b1d09381e5244988ac1bb32074a8190
Juan Pablo Romero <jpablo.romero@gmail.com>
2013-02-12 03:38:53 -0600

    restaurado archivo "orden.py"

--- orden.py
+++ orden.py
@@ -2,16 +2,16 @@
 # ui, modulo, nombre
 
 orden = [
-###    "Presentacion", ## This is added manually in main.py
-#    "Test",
+    ###    "Presentacion", ## This is added manually in main.py
+    #    "Test",
     "Curvas1",
-#    "curvas_superficies",
-#    "Curvas2",
-#    "Curvas3",
-#    "Superficies1",
-#    "Superficies4",
-#    "Superficies2",
-#    "Superficies3",
-#    "CamposVectoriales",
-#    "CurvaturasNormales"
+    "curvas_superficies",
+    "Curvas2",
+    "Curvas3",
+    "Superficies1",
+    "Superficies4",
+    "Superficies2",
+    "Superficies3",
+    "CamposVectoriales",
+    "CurvaturasNormales"
 ]
