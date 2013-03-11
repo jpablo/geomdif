afe7563ee1f1e20e821a8dd941e9d5b3d5f521ce
juan pablo romero <jpablo.romero@gmail.com>
2013-03-10 22:38:42 -0600

    Curvas1.py viewpoint fixes

--- Curvas1.py
+++ Curvas1.py
@@ -4,50 +4,25 @@ from math import *
 from PyQt4 import QtGui
 from pivy.coin import *
 
-from superficie.nodes import Line, Curve3D, Bundle2, Bundle3, PointSet#, Sphere, Arrow
+from superficie.nodes import Line, Curve3D, Bundle2, Bundle3, PointSet
 from superficie.book import Chapter, Page
 from superficie.util import Vec3, _1, partial
-#from superficie.util import intervalPartition
-#from superficie.gui import onOff, CheckBox, Button, SpinBox
 from superficie.widgets import VisibleCheckBox, Slider, SpinBox
 from superficie.plots import ParametricPlot3D
 from superficie.animations import AnimationGroup
 
-def esfera(col):
-    sep = SoSeparator()
 
-#    comp = SoComplexity()
-#    comp.value.setValue(1)
-#    comp.textureQuality.setValue(0.9)
+class PlanePage(Page):
+    def __init__(self, name):
+        Page.__init__(self, name)
+        self.camera_position = (0, 0, 13)
+        self.showAxis(True)
+        self.axis_z.setVisible(False)
 
-    esf = SoSphere()
-    esf.radius = 2.97
 
-    light = SoShapeHints()
-    light.VertexOrdering = SoShapeHints.COUNTERCLOCKWISE
-    light.ShapeType = SoShapeHints.UNKNOWN_SHAPE_TYPE
-    light.FaceType = SoShapeHints.UNKNOWN_FACE_TYPE
-
-    mat = SoMaterial()
-    mat.emissiveColor = col
-    mat.diffuseColor = col
-    mat.transparency.setValue(0.4)
-
-    trans = SoTransparencyType()
-#    trans.value = SoTransparencyType.SORTED_OBJECT_BLEND
-    trans.value = SoTransparencyType.SORTED_OBJECT_SORTED_TRIANGLE_BLEND
-#    trans.value = SoTransparencyType.DELAYED_BLEND
-
-#    sep.addChild(comp)
-    sep.addChild(light)
-    sep.addChild(trans)
-    sep.addChild(mat)
-    sep.addChild(esf)
-
-    return sep
-
-class Tangente(Page):
-    u"""La función tangente establece <b>una biyección</b> <b>entre un segmento abierto y la totalidad de los números reales.</b>
+class Tangente(PlanePage):
+    u"""La función tangente establece <b>una biyección</b> <b>entre un segmento abierto y la totalidad de los \
+    números reales.</b>
     La gráfica de un periodo es una curva infinitamente diferenciable que admite dos <i>asíntotas</i>:
     una a la izquierda y otra a la derecha. Asíntota de una curva es una recta a la cual la curva se acerca tanto como
     se quiera, sin cortarla y teniendo a la recta como límite de la posición de sus tangentes cuando la norma de $(y,
@@ -55,15 +30,13 @@ class Tangente(Page):
     """
 
     def __init__(self):
-        super(Tangente,self).__init__('Tangente')
-        self.camera_position = (0, 0, 10)
-        self.showAxis(True)
-        self.axis_z.setVisible(False)
+        PlanePage.__init__(self, 'Tangente')
         npuntos = 100
         delta = .2
 
         def Tan(t):
             return Vec3(t, tan(t), 0)
+
         def Derivada(t):
             return Vec3(1, 1 / cos(t) ** 2, 0)
 
@@ -81,19 +54,17 @@ class Tangente(Page):
         self.addChild(Line([(pi / 2, -5, 0), (pi / 2, 5, 0)], color=(1, .5, .5)))
 
         tangente = curva1.attachField("tangente", Derivada)
-        tangente.add_tail(radius = 0.08)
+        tangente.add_tail(radius=0.08)
         self.setupAnimations([tangente])
 
-    # Dibuja la loxodroma y la esfera
 
-class ValorAbsoluto(Page):
+class ValorAbsoluto(PlanePage):
     u"""El valor absoluto es una función continua de su variable pero
     <b>no diferenciable en $0$</b> pues a la  izquierda del origen su gráfica tiene pendiente constante
     $-1$ y a la derecha su pendiente es constante $1$, por eso no puede existir el vector tangente en $(0,0)$."""
+
     def __init__(self):
-        Page.__init__(self, u"Valor absoluto")
-        self.showAxis(True)
-        self.axis_z.setVisible(False)
+        PlanePage.__init__(self, u"Valor absoluto")
 
         def Abs(t):
             return Vec3(t, abs(t), 0)
@@ -107,24 +78,26 @@ class ValorAbsoluto(Page):
         self.addChild(curva1)
 
         tangente = curva1.attachField("tangente", Derivada)
-        tangente.add_tail(radius = 0.08)
+        tangente.add_tail(radius=0.08)
         self.setupAnimations([tangente])
 
 
-class Cusp(Page):
+class Cusp(PlanePage):
     u"""Esta curva parametrizada tiene bien definido el vector tangente para cualquier valor del parámetro, pero
-    en el punto de corte hay dos vectores tangentes, distinguidos por el parámetro. <b>La traza de la curva no es admisible como $1$-variedad diferenciable</b>.
+    en el punto de corte hay dos vectores tangentes, distinguidos por el parámetro. <b>La traza de la curva no es
+    admisible como $1$-variedad diferenciable</b>.
     """
+
     def __init__(self):
-        Page.__init__(self, u"Curva diferenciable con autointersección")
-        self.showAxis(True)
-        self.axis_z.setVisible(False)
-        def cusp(t): return Vec3(t ** 3 - 4 * t, t ** 2 - 4, 0)
-        curva1 = Curve3D(cusp, (-2.5, 2.5, 200), width=2)
-        self.addChild(curva1)
-        tangente = curva1.attachField("tangente", lambda t: Vec3(-4 + 3 * t ** 2, 2 * t, 0))
-        tangente.add_tail(radius = 0.08)
-        self.setupAnimations([tangente])
+        PlanePage.__init__(self, u"Curva diferenciable con autointersección")
+
+        def cusp(t):
+            return Vec3(t ** 3 - 4 * t, t ** 2 - 4, 0)
+        curve1 = Curve3D(cusp, (-2.5, 2.5, 200), width=2)
+        self.addChild(curve1)
+        tangent = curve1.attachField("tangente", lambda t: Vec3(-4 + 3 * t ** 2, 2 * t, 0))
+        tangent.add_tail(radius=0.08)
+        self.setupAnimations([tangent])
 
 
 class Alabeada(Page):
@@ -138,6 +111,8 @@ class Alabeada(Page):
     """
     def __init__(self):
         Page.__init__(self, "Alabeada")
+        self.camera_position = (10, 10, 10)
+        self.camera_viewAll = True
         self.setupPlanes()
         c = lambda t: Vec3(t, t ** 2, t ** 3)
         altura = -1
@@ -148,20 +123,20 @@ class Alabeada(Page):
         curvas = [curva, lxy, lxz, lyz]
         self.showAxis(False)
         self.addChildren(curvas)
-        self.setupAnimations([ AnimationGroup(curvas, (5000,0,len(curva)-1)) ])
+        self.setupAnimations([AnimationGroup(curvas, (5000, 0, len(curva) - 1))])
 
 
 class Curvas1(Chapter):
     def __init__(self):
         Chapter.__init__(self, name="Ejemplos de curvas planas")
 
-        figuras = [
+        pages = [
             Tangente,
             ValorAbsoluto,
             Cusp,
             Alabeada,
         ]
-        for f in figuras:
+        for f in pages:
             self.addPage(f())
 
 if __name__ == "__main__":
@@ -179,4 +154,3 @@ if __name__ == "__main__":
     visor.chaptersStack.show()
     visor.notasStack.show()
     sys.exit(app.exec_())
-
